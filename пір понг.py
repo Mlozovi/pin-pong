b9bb4255ef115da2fe82a576c3c4a5520f24b6d3
Mlozovi <126389024+Mlozovi@users.noreply.github.com>
2023-03-05 13:17:43 +0200

    Add files via upload

---
+++ пір понг.py
@@ -0,0 +1,24 @@
+ from pygame import *
+
+
+
+ class GameSprite(sprite.Sprite):
+
+     def __init__(self, player_image, plaer_x, player_speed, wight, height):
+         super()/__init__()
+
+         self.image = transform.scale(image.load(player_image), (wight, height))
+         self.speed = player_speed
+
+         self.rect = self.image.get_rect()
+         self.rect.x = plaer_x
+         self.rect.y = plaer_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
